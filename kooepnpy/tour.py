81204595ae5ff6c692eb5f869d2bda18b81603ee
drtagkim <masan.korea@gmail.com>
2018-11-25 14:31:44 +0900

    tour.py 추가
    KorTourRegionCode
    KorTourKeywordSearch
    KorTourAreaCode
    KorTourCategoryCode
    KorTourSearchFestival
    KorTourSearchStay
    KorTourAreaBasedList
    KorTourLocationBasedList
    KorTourDetailIntro
    KorTourDetailImage

--- kooepnpy/tour.py
+++ kooepnpy/tour.py
@@ -14,6 +14,20 @@ class KorTour:
         self.nogomsg="Service is not available."
         self.service_ok=False #for assertion
         self.r=None #python requests
+        self.contentType=self.create_content_type()
+    def create_content_type(self):
+        contentType={}
+        contentType['attraction']=12 #관광지
+        contentType['culture']=14 #문화시설
+        contentType['festival']=15 #축제공연행사
+        contentType['cource']=25 #여행코스
+        contentType['reports']=28 #레포츠
+        contentType['accomodation']=32 #숙박
+        contentType['shopping']=38 #쇼핑
+        contentType['food']=39 #음식
+        return contentType
+    def show_content_type(self):
+        return self.contentType.keys()
     def access_data(self):
         assert self.service_ok, self.nogomsg
         r=self.r
@@ -38,7 +52,12 @@ class KorTour:
         try:
             i=o['response']['body']['items']['item']
             j=json.dumps(i)
-            return pd.read_json(j)
+            if isinstance(i,list):
+                return pd.read_json(j)
+            else:
+                return pd.read_json('['+j+']')
+        except KeyError:
+            return None
         except TypeError:
             return None
     def get_page_no(self):
@@ -73,6 +92,15 @@ class KorTourKeywordSearch(KorTour):
         params['keyword']=keyword
         url=self.endpoint+self.service_url
         super().call_request(url,params)
+class KorTourAreaCode(KorTour):
+    def __init__(self,service_key):
+        super().__init__(service_key)
+        self.service_url="/areaCode"
+    def collect(self,areacode=''):
+        params=super().collect()
+        params['areaCode']=areacode
+        url=self.endpoint+self.service_url
+        super().call_request(url,params)
 class KorTourCategoryCode(KorTour):
     def __init__(self,service_key):
         super().__init__(service_key)
@@ -90,14 +118,55 @@ class KorTourSearchFestival(KorTour):
         params['eventStartDate']=event_start_date
         url=self.endpoint+self.service_url
         super().call_request(url,params)
-class KorTourAreaCode(KorTour):
+class KorTourSearchStay(KorTour):
     def __init__(self,service_key):
         super().__init__(service_key)
-        self.service_url="/areaCode"
-    def collect(self,areacode=''):
+        self.service_url="/searchStay"
+    def collect(self,area_code='',sigungu_code=''):
         params=super().collect()
-        params['areaCode']=areacode
+        params['areaCode']=area_code
+        params['sigunguCode']=sigungu_code
         url=self.endpoint+self.service_url
         super().call_request(url,params)
-        
-
+class KorTourAreaBasedList(KorTour):
+    def __init__(self,service_key):
+        super().__init__(service_key)
+        self.service_url="/areaBasedList"
+    def collect(self,area_code='',sigungu_code='',content_type_id=''):
+        params=super().collect()
+        params['areaCode']=area_code
+        params['sigunguCode']=sigungu_code
+        params['contentTypeId']=content_type_id
+        url=self.endpoint+self.service_url
+        super().call_request(url,params)
+class KorTourLocationBasedList(KorTour):
+    def __init__(self,service_key):
+        super().__init__(service_key)
+        self.service_url="/locationBasedList"
+    def collect(self,longitude=126.5835,latitude=37.1233,radius=5000,content_type_id=''):
+        params=super().collect()
+        params['contentTypeId']=content_type_id
+        params['mapX']=longitude
+        params['mapY']=latitude
+        params['radius']=radius
+        url=self.endpoint+self.service_url
+        super().call_request(url,params)
+class KorTourDetailIntro(KorTour):
+    def __init__(self,service_key):
+        super().__init__(service_key)
+        self.service_url="/detailIntro"
+    def collect(self,content_id=1118680,content_type_id=15):
+        params=super().collect()
+        params['contentId']=content_id
+        params['contentTypeId']=content_type_id
+        url=self.endpoint+self.service_url
+        super().call_request(url,params)
+class KorTourDetailImage(KorTour):
+    def __init__(self,service_key):
+        super().__init__(service_key)
+        self.service_url="/detailImage"
+    def collect(self,content_id=1118680):
+        params=super().collect()
+        params['contentId']=content_id
+        url=self.endpoint+self.service_url
+        super().call_request(url,params)
