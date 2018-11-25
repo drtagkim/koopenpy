2c858009473840158a25d8b63a81600f032a7041
drtagkim <masan.korea@gmail.com>
2018-11-25 12:59:36 +0900

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name='koopenpy',
+    version='0.0.1',
+    description='Korean Open API frontend for lecture',
+    author='Taekyung Kim',
+    author_email='kimtk@office.suwon.ac.kr',
+    url='https://github.com/drtagkim/koopenpy'
+)
