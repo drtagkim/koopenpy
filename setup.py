346215327adc8e262ca5dd4999251c5ac01351da
drtagkim <masan.korea@gmail.com>
2018-11-25 14:32:11 +0900

    setup.py
    version up

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='koopenpy',
-    version='0.1',
+    version='0.1.1',
     description='Korean Open API frontend for lecture',
     author='Taekyung Kim',
     author_email='kimtk@office.suwon.ac.kr',
